18872a4ee3d197b589e6fa44dbe79ab9f8af966e
조수연 <48742378+su6378@users.noreply.github.com>
2022-09-02 15:36:54 +0900

    프로그래머스 : 최소직사각형
    #완전탐색

---
+++ level1/실패율.kt
@@ -0,0 +1,32 @@
+
+class Solution {
+   fun solution(N: Int, stages: IntArray): IntArray {
+    val stage = IntArray(N + 2)
+    var user = stages.size
+    val failPair = ArrayList<Pair<Int, Double>>()
+    for (i in stages.indices) {
+        stage[stages[i]]++
+    }
+
+    for (i in 1..N) {
+        if (user == 0) {
+            failPair.add(Pair(i, 0.0))
+        } else {
+            var fail = stage[i].toDouble() / user.toDouble()
+            failPair.add(Pair(i, fail))
+            user -= stage[i]
+        }
+    }
+
+    failPair.sortWith(compareByDescending { it.second })
+
+
+    var answer = IntArray(N)
+
+    for (i in failPair.indices) {
+        answer[i] = failPair.get(i).first
+    }
+    return answer
+}
+
+}
