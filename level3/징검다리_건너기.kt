76602b7ea3d3ef256798fba8ef9e664fb5b6433b
su6378 <josooyeon11@naver.com>
2023-09-27 11:38:12 +0900

    프로그래머스 : 징검다리 건너기
    # 이분탐색

---
+++ level3/징검다리_건너기.kt
@@ -0,0 +1,36 @@
+import kotlin.math.max
+
+fun main() {
+    SolutionKotlin().solution(stones = intArrayOf(2, 4, 5, 3, 2, 1, 4, 2, 5, 1), k = 3)
+}
+
+class SolutionKotlin {
+    fun solution(stones: IntArray, k: Int): Int {
+        var answer = 0
+        var min = 1
+        var max = 20000000
+
+        while (min <= max) {
+            var mid = (min + max) / 2
+
+            if (canCross(k, stones, mid)) {
+                min = mid + 1
+                answer = max(answer, mid)
+            } else max = mid - 1
+        }
+
+        return answer
+    }
+
+    fun canCross(k: Int, stones: IntArray, people: Int): Boolean {
+        var cnt = 0
+
+        for (stone in stones) {
+            if (stone < people) cnt++
+            else cnt = 0
+
+            if (cnt == k) return false
+        }
+        return true
+    }
+}
